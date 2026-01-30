f0f6a72a0e2f8bb234c752143d76491d87aecc34
frankayubi <frankayubi10@gmail.com>
2026-01-30 23:08:04 +0000

    added color

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,59 @@
-// Task 8: Create your About component here
 import IMG from '../assets/Detective.png';
+import MTLogo from '../assets/mtbank-logo.png';
+import QAimereLogo from '../assets/qaimere-logo.png';
+import ChubbLogo from '../assets/chubb-logo.png';
 
 const About = () => {
     return (
         <div id="about" className="about">
             <h1 className="about-heading">About Me</h1>
-            <div className="about-info">
-                <p className="about-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi suscipit quidem ullam voluptates repellendus explicabo placeat, expedita ratione saepe eaque dolorum sed deleniti, odit numquam, nostrum tempore quos corrupti rem.</p>
-                <div className="about-img">
-                    <div className="about-img-wrapper">
-                        <img src={IMG} alt="Detective" />
+            <div className="about-container">
+                <div className="about-info">
+                    {/* Left Side: Bio and Experience */}
+                    <div className="about-left">
+                        <p className="about-desc">
+                            Hi, I'm Frank Yubi. I recently finished my Computer Science degree at NJIT. 
+                            I have hands-on experience building backend services and optimizing code. 
+                            I enjoy the challenge of solving complex problems behind the scenes and 
+                            I'm currently looking for a full-time Software Engineering position to 
+                            put my skills to work.
+                        </p>
+
+                        <div className="experience-list">
+                            <div className="experience-item">
+                                <img src={MTLogo} alt="M&T Bank" className="exp-logo" />
+                                <div className="exp-text">
+                                    <h3>M&T Bank</h3>
+                                    <p>Software Engineering Intern</p>
+                                </div>
+                            </div>
+                            <div className="experience-item">
+                                <img src={QAimereLogo} alt="QAimere" className="exp-logo" />
+                                <div className="exp-text">
+                                    <h3>QAimere Technologies</h3>
+                                    <p>Developer</p>
+                                </div>
+                            </div>
+                            <div className="experience-item">
+                                <img src={ChubbLogo} alt="Chubb" className="exp-logo" />
+                                <div className="exp-text">
+                                    <h3>Chubb</h3>
+                                    <p>Data Engineer Intern</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Right Side: Detective Image */}
+                    <div className="about-right">
+                        <div className="about-img-wrapper">
+                            <img src={IMG} alt="Detective" />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default About;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,34 @@
-// Importing the GitHub and LinkedIn icons rom react-icons/fa and an avatar image from the assets folder
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFileDownload } from "react-icons/fa"; // Added download icon
 import Avatar from '../assets/Software.png';
 
-// Defining the Body functional component
 const Body = () => {
     return (
-        // Main container for the body section
         <div id="body" className="body">
             <div className="body-container">
-
-               {/* Profile section containing the avatar, name, title, and social icons */}
                 <div className="body-profile">
                     <img className="body-img" alt='avatar' src={Avatar} />
 
-                    {/* Container for user details */}
                     <div className="body-content">
                         <div className="body-headline">Frank Yubi</div>
                         <div className="body-text">Software Engineer</div>
+                        
                     </div>
 
-                    {/* Social media icons */}
                     <div className="body-icons">
-                        <a href="https://www.educative.io/" target="_blank" rel="noreferrer" className="icon-link"><i><FaGithub /></i> </a>
-                        <a href="https://www.educative.io/" target="_blank" rel="noreferrer" className="icon-link"><i><FaLinkedin /></i></a>
+                        <a href="https://github.com/frankayubi" target="_blank" rel="noreferrer" className="icon-link"><i><FaGithub /></i> </a>
+                        <a href="https://www.linkedin.com/frankayubi" target="_blank" rel="noreferrer" className="icon-link"><i><FaLinkedin /></i></a>
                     </div>
+
+                        {/* New Download Button */}
+                        <div className="body-cta">
+                            <a 
+                                href="/Franky_Yubi_Resume.pdf" 
+                                className="download-btn" 
+                                download="Franky_Yubi_Resume.pdf"
+                            >
+                                Download Resume <FaFileDownload style={{ marginLeft: '8px' }} />
+                            </a>
+                        </div>
                 </div>
             </div>
         </div>
